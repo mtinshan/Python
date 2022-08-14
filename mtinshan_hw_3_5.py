61807d030119b7be8d47b8fa6cc78c38187280e8
mtinshan <svitak27@gmail.com>
2022-08-14 20:43:18 +0800

    hw_3
    hw_3_5

---
+++ mtinshan_hw_3_5.py
@@ -0,0 +1,34 @@
+from random import choice
+
+nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
+adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
+list_1 = []
+
+def get_jokes(n, flag=False):
+    for i in range(n):
+        random_noun = choice(nouns)
+        random_adverb = choice(adverbs)
+        random_adjective = choice(adjectives)
+        joke = f'{random_noun} {random_adverb} {random_adjective}'
+        list_2 = []
+        print(joke)
+        if flag == True:
+            list_2 = joke.split()
+            for noun in nouns:
+                for fun in list_2:
+                    if noun == fun:
+                        nouns.remove(noun)
+
+            for adverb in adverbs:
+                for fun in list_2:
+                    if adverb == fun:
+                        adverbs.remove(adverb)
+
+
+            for adjective in adjectives:
+                for fun in list_2:
+                    if adjective == fun:
+                        adjectives.remove(adjective)
+
+get_jokes(n = int(input('Введите число: ')), flag=True)
